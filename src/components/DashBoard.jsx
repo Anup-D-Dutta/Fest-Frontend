2d19b4a7dd704664d37aa20306baab1f9b015909
Adityaverma510 <av4014032@gmail.com>
2025-06-13 16:38:58 +0530

    refactor: enhance DashBoard component layout and content

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const DashBoard = () => {
-  return (
-    <div><h1>DashBoard</h1><p>Welcome to the dashboard!</p></div>
-  )
+    return (
+        <div><h1>DashBoard</h1>
+        <p>Welcome to the dashboard!</p>
+        <p>This is the dashboard</p>
+        </div>
+    )
 }
 
 export default DashBoard;
